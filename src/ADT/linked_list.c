2a5754d4fb93cc0ab9d9a9018d0fea313a457579
SapphireDragoness <linda.monfermoso@gmail.com>
2023-10-03 09:40:04 +0200

    modified:   src/ADT/linked_list.c

--- src/ADT/linked_list.c
+++ src/ADT/linked_list.c
@@ -46,7 +46,7 @@ int linked_list_clear(linked_list_t linked_list, int destroy_data) {
     return 0;
 }
 
-int linked_list_ins_next(linked_list_t linked_list, void *data) {
+int linked_list_ins_next(linked_list_t linked_list, linked_list_node_t target, void *data) {
     if(linked_list == NULL) {
         fprintf(stderr, "Linked list not allocated.")
         return -1;
@@ -55,15 +55,65 @@ int linked_list_ins_next(linked_list_t linked_list, void *data) {
     linked_list_node_t *node = malloc(sizeof(linked_list_node_t));
 
     node->data = data;
-    node -> next = 
+    node->next = NULL;
+
+    if(linked_list->head == NULL) {
+        linked_list->tail = node;
+        node->next = linked_list->head;
+        linked_list->head = node;
+    }
+    else {
+        node->next = target->next;
+        target->next = node;
+    }
+
+    linked_list->size += 1;
+
+    return 0;
 }
 
-int linked_list_rem_next(linked_list_t linked_list, int destroy_data);
+int linked_list_rem_next(linked_list_t linked_list, linked_list_node_t target, int destroy_data);
 
-void* linked_list_head(const linked_list_t list);
+void* linked_list_head(const linked_list_t list) {
+    if(linked_list == NULL) {
+        fprintf(stderr, "Linked list not allocated.")
+        return -1;
+    }
 
-void* linked_list_tail(const linked_list_t list);
+    if(linked_list_is_empty) {
+        return NULL;
+    }
 
-size_t linked_list_size(const linked_list_t list);
+    return linked_list->head->data;
+}
 
-int linked_list_is_empty(const linked_list_t list);
+void* linked_list_tail(const linked_list_t list) {
+    if(linked_list == NULL) {
+        fprintf(stderr, "Linked list not allocated.")
+        return -1;
+    }
+
+    if(linked_list_is_empty) {
+        return NULL;
+    }
+
+    return linked_list->tail->data;
+}
+
+size_t linked_list_size(const linked_list_t list) {
+    if(linked_list == NULL) {
+        fprintf(stderr, "Linked list not allocated.")
+        return -1;
+    }
+
+    return linked_list->size;
+}
+
+int linked_list_is_empty(const linked_list_t list) {
+    if(linked_list == NULL) {
+        fprintf(stderr, "Linked list not allocated.")
+        return -1;
+    }
+
+    return linked_list->size == 0 ? 1 : 0;
+}
